c2fe4aba228d5aa47c3022a0f08929b7f34ed195
Daria Eliseeva <elisey422das@gmail.com>
2024-01-15 15:49:32 +0100

    working js

--- todo.js
+++ todo.js
@@ -2,11 +2,23 @@ let todoLine = document.querySelector('.todo-form');
 let todoMessage = document.querySelector('#todo-template').content;
 let container = document.querySelector('.main-content');
 let newMessageTemplate = todoMessage.querySelector('.todo-cell');
-let newTodoLine = todoLine.querySelector('.todo-form-input');
+let newTodoText = todoLine.querySelector('.todo-form-input');
 
 todoLine.addEventListener('submit', function (evt) {
     evt.preventDefault();
-    let lineMessage = newTodoLine.value;
-})
+    let lineMessage = newTodoText.value;
+    let message = newMessageTemplate.cloneNode(true);
+    console.log(message);
+    let messageContent = message.querySelector('p');
+    messageContent.textContent = lineMessage;
+    let buttonDelete = message.querySelector('.todo-delete-button');
+    buttonDelete.addEventListener('click', function (evt) {
+        evt.preventDefault()
+        message.remove();
+
+    })
+    container.appendChild(message);
+    newTodoText.value = '';
+});
 
 console.log('hhhh')
