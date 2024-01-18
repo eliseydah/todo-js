dec6a941a0a9ef0bfb58c53acea6b4af2aee585a
Daria Eliseeva <elisey422das@gmail.com>
2024-01-18 09:32:13 +0100

    was changed checkmarks

--- todo.js
+++ todo.js
@@ -8,6 +8,7 @@ todoLine.addEventListener('submit', function (evt) {
     evt.preventDefault();
     let lineMessage = newTodoText.value;
     let message = newMessageTemplate.cloneNode(true);
+    // Element is created
     console.log(message);
     let messageContent = message.querySelector('p');
     messageContent.textContent = lineMessage;
@@ -17,8 +18,24 @@ todoLine.addEventListener('submit', function (evt) {
         message.remove();
 
     })
+
+
     container.appendChild(message);
+    let checkThrough = message.querySelector('.todo-checkbox')
+    let lineThrough = message.querySelector('.todo-text');
+    checkThrough.addEventListener('change', function (evt) {
+        evt.preventDefault()
+        if (checkThrough.checked === true) {
+            lineThrough.classList.add('line-through');
+        } else {
+            lineThrough.classList.remove('line-through');
+        }
+
+
+    })
     newTodoText.value = '';
 });
+
+
 
 console.log('hhhh')
