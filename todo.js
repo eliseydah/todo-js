f3b1b01c05c096c741211e16c5779ec825fcaa11
Daria Eliseeva <elisey422das@gmail.com>
2024-01-18 15:56:07 +0100

    working counter

--- todo.js
+++ todo.js
@@ -3,6 +3,8 @@ let todoMessage = document.querySelector('#todo-template').content;
 let container = document.querySelector('.cell-content');
 let newMessageTemplate = todoMessage.querySelector('.todo-cell');
 let newTodoText = todoLine.querySelector('.todo-form-input');
+let counter = 0;
+let clearCounter = 0;
 
 todoLine.addEventListener('submit', function (evt) {
     evt.preventDefault();
@@ -19,23 +21,44 @@ todoLine.addEventListener('submit', function (evt) {
 
     })
 
-
     container.appendChild(message);
+    counter++;
+    document.getElementById('elementCounter').textContent = counter;
+
     let checkThrough = message.querySelector('.todo-checkbox')
     let lineThrough = message.querySelector('.todo-text');
     checkThrough.addEventListener('change', function (evt) {
         evt.preventDefault()
         if (checkThrough.checked === true) {
             lineThrough.classList.add('line-through');
+            counter--;
+            document.getElementById('elementCounter').textContent = counter;
+            clearCounter++;
+            document.getElementById('clearElementCounter').textContent = clearCounter;
         } else {
             lineThrough.classList.remove('line-through');
+            counter++;
+            document.getElementById('elementCounter').textContent = counter;
+            clearCounter--;
+            document.getElementById('clearElementCounter').textContent = clearCounter;
         }
-
-
     })
+
     newTodoText.value = '';
 });
 
+
+
+let activeButton = document.querySelector('.active-button');
+activeButton.addEventListener('change', function (evt) {
+    evt.preventDefault()
+    let completedArray = document.querySelectorAll('.line-through');
+    for (let i = 0; i < completedArray.length; i++) {
+        let oneComplete = completedArray[i];
+        oneComplete.classList.add('hidden');
+    }
+
+})
 
 
 console.log('hhhh')
