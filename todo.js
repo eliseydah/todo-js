8eba070bc10099195408142370736eb4254a62d4
Daria Eliseeva <elisey422das@gmail.com>
2024-01-22 13:37:55 +0100

    all counters work correctly

--- todo.js
+++ todo.js
@@ -18,6 +18,10 @@ todoLine.addEventListener('submit', function (evt) {
     buttonDelete.addEventListener('click', function (evt) {
         evt.preventDefault()
         message.remove();
+        clearCounter--;
+        document.getElementById('clearElementCounter').textContent = clearCounter;
+        counter--;
+        document.getElementById('elementCounter').textContent = counter;
 
     })
 
@@ -31,12 +35,14 @@ todoLine.addEventListener('submit', function (evt) {
         evt.preventDefault()
         if (checkThrough.checked === true) {
             lineThrough.classList.add('line-through');
+            message.classList.add('line-through')
             counter--;
             document.getElementById('elementCounter').textContent = counter;
             clearCounter++;
             document.getElementById('clearElementCounter').textContent = clearCounter;
         } else {
             lineThrough.classList.remove('line-through');
+            message.classList.remove('line-through');
             counter++;
             document.getElementById('elementCounter').textContent = counter;
             clearCounter--;
@@ -47,15 +53,44 @@ todoLine.addEventListener('submit', function (evt) {
     newTodoText.value = '';
 });
 
-
-
 let activeButton = document.querySelector('.active-button');
 activeButton.addEventListener('change', function (evt) {
-    evt.preventDefault()
+    evt.preventDefault();
+    let allVision = document.querySelectorAll('.hidden');
+    for (let i = 0; i < allVision.length; i++) {
+        let visionElement = allVision[i];
+        visionElement.classList.remove('hidden');
+    }
     let completedArray = document.querySelectorAll('.line-through');
     for (let i = 0; i < completedArray.length; i++) {
         let oneComplete = completedArray[i];
         oneComplete.classList.add('hidden');
+    }
+
+})
+
+let completedButton = document.querySelector('.completed-button');
+completedButton.addEventListener('change', function (evt) {
+    evt.preventDefault()
+    let allVision = document.querySelectorAll('.hidden');
+    for (let i = 0; i < allVision.length; i++) {
+        let visionElement = allVision[i];
+        visionElement.classList.remove('hidden');
+    }
+    let notCompletedArray = document.querySelectorAll('.todo-cell:not(.line-through)');
+    for (let i = 0; i < notCompletedArray.length; i++) {
+        let oneNotCompleted = notCompletedArray[i];
+        oneNotCompleted.classList.add('hidden');
+    }
+})
+
+let allButton = document.querySelector('.all-button');
+allButton.addEventListener('change', function (evt) {
+    evt.preventDefault()
+    let clear = document.querySelectorAll('.hidden');
+    for (let i = 0; i < clear.length; i++) {
+        let clearElement = clear[i];
+        clearElement.classList.remove('hidden');
     }
 
 })
