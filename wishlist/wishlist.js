0739c11af63fa72ce21f7a380a9b1796a9f63b26
Daria Eliseeva <elisey422das@gmail.com>
2024-02-06 14:11:31 +0100

    input form was corrected

--- wishlist/wishlist.js
+++ wishlist/wishlist.js
@@ -47,6 +47,16 @@ function categoryChoice(wishElement) {
 
 }
 
+function refreshArray(hiddenArray, normalArray) {
+    for (let i = 0; i < hiddenArray.length; i++) {
+        let oneHiddenElement = hiddenArray[i];
+        oneHiddenElement.classList.add('unvisible')
+    }
+    for (let i = 0; i < normalArray.length; i++) {
+        let normalElement = normalArray[i];
+        normalElement.classList.remove('unvisible');
+    }
+}
 
 
 let filterButton = document.querySelector('.filter-choice');
@@ -57,31 +67,46 @@ filterButton.addEventListener('change', function (evt) {
     let category3Array = document.querySelectorAll('.cat-3');
     let priceSelect = document.getElementById('filter-price-select');
     let priceChoice = priceSelect.value;
+    if (priceChoice === 'filter') {
+        let allVisibleArray = document.querySelectorAll('.wish-container')
+        for (let i = 0; i < allVisibleArray.length; i++) {
+            allVisibleArray[i].classList.remove('unvisible');
+        }
+    }
     if (priceChoice === 'filter-price-1') {
         let newCat1Array = document.querySelectorAll('.wish-container:not(.cat-1)');
+        let cat1Array = document.querySelectorAll('.cat-1');
         console.log(newCat1Array)
-        for (let i = 0; i < newCat1Array.length; i++) {
-            let oneHiddenElement = newCat1Array[i];
-            oneHiddenElement.classList.add('unvisible')
-        }
+        refreshArray(newCat1Array, cat1Array);
+        // for (let i = 0; i < newCat1Array.length; i++) {
+        //     let oneHiddenElement = newCat1Array[i];
+        //     oneHiddenElement.classList.add('unvisible')
+        // }
+        // for (let i = 0; i < cat1Array.length; i++) {
+        //     cat1Array[i].classList.remove('unvisible');
+        // }
 
     }
     if (priceChoice === 'filter-price-2') {
         let newCat2Array = document.querySelectorAll('.wish-container:not(.cat-2)');
         console.log(newCat2Array)
-        for (let i = 0; i < newCat2Array.length; i++) {
-            let oneHiddenElement = newCat2Array[i];
-            oneHiddenElement.classList.add('unvisible')
-        }
+        let Cat2Array = document.querySelectorAll('.cat-2')
+        refreshArray(newCat2Array, category2Array);
+        // for (let i = 0; i < newCat2Array.length; i++) {
+        //     let oneHiddenElement = newCat2Array[i];
+        //     oneHiddenElement.classList.add('unvisible')
+        // }
 
     }
     if (priceChoice === 'filter-price-3') {
         let newCat3Array = document.querySelectorAll('.wish-container:not(.cat-3)');
         console.log(newCat3Array)
-        for (let i = 0; i < newCat3Array.length; i++) {
-            let oneHiddenElement = newCat3Array[i];
-            oneHiddenElement.classList.add('unvisible')
-        }
+        let cat3Array = document.querySelectorAll('.cat-3');
+        refreshArray(newCat3Array, cat3Array);
+        // for (let i = 0; i < newCat3Array.length; i++) {
+        //     let oneHiddenElement = newCat3Array[i];
+        //     oneHiddenElement.classList.add('unvisible')
+        // }
     }
 })
 
