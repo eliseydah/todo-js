116ab101c90c3f13bad64173f4509aac12392c9a
Daria Eliseeva <elisey422das@gmail.com>
2024-04-22 14:53:44 +0200

    styles was moved to item

--- vue-todo/src/components/Item.vue
+++ vue-todo/src/components/Item.vue
@@ -18,3 +18,52 @@ function onButtonClick() {
         <!-- @click="removeTodo(todo)" -->
     </div>
 </template>
+<style scoped>
+.done {
+    text-decoration: line-through;
+}
+
+.todo-checkbox {
+    display: none;
+}
+
+.todo-delete-button {
+    font-size: 20px;
+    padding-right: 10px;
+    border: 0;
+    color: rgb(210, 116, 147);
+    background-color: white;
+    margin-right: 10px;
+    opacity: 0;
+}
+
+.panel:hover .todo-delete-button {
+    opacity: 1;
+}
+
+.panel label span {
+    height: 12px;
+    width: 12px;
+    border: 1px solid rgb(67, 216, 159);
+    border-radius: 100%;
+    display: inline-block;
+    position: relative;
+    background-color: #FFF;
+    padding: 3px;
+}
+
+[type=checkbox]:checked+span:before {
+    /* <-- ставим иконку, когда чекбокс включен  */
+    content: '\2713';
+    position: absolute;
+    top: -5px;
+    left: 2px;
+    font-size: 20px;
+    color: rgb(141, 235, 199);
+}
+
+.line-through {
+    text-decoration: line-through;
+    color: rgb(184, 180, 180);
+}
+</style>
