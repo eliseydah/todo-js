453dfccec897699a8902a460b3e02637fc9b2efe
Daria Eliseeva <elisey422das@gmail.com>
2025-03-20 17:59:24 +0100

    create home component

---
+++ react/react-plants/src/Header.jsx
@@ -0,0 +1,58 @@
+import "./Header.css";
+function Header() {
+  return (
+    <header className="container">
+      <div className="logo">
+        <img src="../images/plant.svg" alt="logo" height="40px" width="40px" />
+        <p> Plant Shop </p>
+      </div>
+      <ul className="navigation-bar">
+        <li>
+          <a href="">
+            <img
+              src="../images/home.svg"
+              alt="logo"
+              height="25px"
+              width="25px"
+            />
+            <span>Home</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <img
+              src="../images/menu.svg"
+              alt="logo"
+              height="25px"
+              width="25px"
+            />
+            <span>Catalogue</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <img
+              src="../images/heart.svg"
+              alt="logo"
+              height="25px"
+              width="25px"
+            />
+            <span>Wishlist</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <img
+              src="../images/basket.svg"
+              alt="logo"
+              height="25px"
+              width="25px"
+            />
+            <span>Basket</span>
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
+}
+export default Header;
---
+++ react/react-plants/src/Home.jsx
@@ -0,0 +1,14 @@
+import "./Home.css";
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="intro">
+        <h2>Welcome to Plant Shop</h2>
+        <p>Find the perfect plant for your home</p>
+        <p> We help create a cozy space for living and relaxation.</p>
+        <button className="catalogue-button"> open catalogue</button>
+      </div>
+    </div>
+  );
+}
+export default Home;
